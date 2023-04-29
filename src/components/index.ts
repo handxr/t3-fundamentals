d4fd11ba02f42aa92601840963fe81cc25c6dcd5
Han <hander.juan@gmail.com>
2023-04-29 19:04:17 +0200

    profile page and ssg

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
 export * from "./index";
 export * from "./loading";
+export * from "./layout";
---
+++ src/server/api/routers/profile.ts
@@ -0,0 +1,30 @@
+import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
+import { type User } from "@clerk/nextjs/dist/api";
+import { clerkClient } from "@clerk/nextjs/server";
+import { TRPCError } from "@trpc/server";
+import { z } from "zod";
+
+const filterUserForClient = (user: User) => ({
+  id: user.id,
+  username: user.username,
+  profilePicture: user.profileImageUrl,
+});
+
+export const profileRouter = createTRPCRouter({
+  getUserByUsername: publicProcedure
+    .input(z.object({ username: z.string() }))
+    .query(async ({ input }) => {
+      const [user] = await clerkClient.users.getUserList({
+        username: [input.username],
+      });
+
+      if (!user) {
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: "User not found",
+        });
+      }
+
+      return filterUserForClient(user);
+    }),
+});
