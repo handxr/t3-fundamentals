f77333cf44d35c72e19861d3bb8b7d9422699a31
Han <hander.juan@gmail.com>
2023-04-29 20:24:02 +0200

    props

--- src/components/post-view.tsx
+++ src/components/post-view.tsx
@@ -6,27 +6,5 @@ import Link from "next/link";
 
 type PostWithUser = RouterOutputs["posts"]["getAll"][number];
 export const PostView = (props: PostWithUser) => {
-  return (
-    <li
-      key={props.post.id}
-      className="flex items-center gap-4 border-b border-slate-400 p-8"
-    >
-      {/* <Image
-        src={props.author.profilePicture ?? ""}
-        alt="Profile Image"
-        className="h-12 w-12 rounded-full"
-        width={48}
-        height={48}
-      />
-      <div className="flex flex-col">
-        <div className="flex gap-2">
-          <Link href={`/@${props.author.username}`}>
-            <span className="font-bold text-slate-300">{`@${props.author.username}`}</span>{" "}
-          </Link>
-          ·{" "}
-        </div>
-        <span>{props.post.content}</span>
-      </div> */}
-    </li>
-  );
+  return <></>;
 };
