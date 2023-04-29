f6ea7d219b3c52b82a95364320017f7f312b3891
Han <hander.juan@gmail.com>
2023-04-29 20:10:55 +0200

    laout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,10 +1,10 @@
 import { type PropsWithChildren } from "react";
 
-export const Layout = (props: PropsWithChildren) => {
+export const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="flex min-h-screen justify-center ">
       <div className="w-full border-x border-slate-400  md:max-w-2xl">
-        {props.children}
+        {children}
       </div>
     </main>
   );
